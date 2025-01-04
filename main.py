9c352ebf89dff59a7f3952b547723d40096640a7
Mikeneqa <mikeneqa@gmail.com>
2025-01-04 12:57:30 -0700

    update

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from signal import pause
 class GPIOHandler:
     def __init__(self, input_pins):
         self.input_pins = input_pins
-        self.buttons = list()
+        self.buttons = []
 
         for label, pin in input_pins.items():
             try:
